46d1ad660593d7f6d57b75b91ec2b641366a1179
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-15 07:52:34 +0530

    Cli command for stating function.

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/yumyum-pi/txtBP/internal"
+)
+
+var e string        // file extention to search for
+var d string = "./" // directory to find the file in
+var s string = "-"  // seperator of the line
+
+// create the root cobra command
+var rootCmd = &cobra.Command{
+	Use: "txtBP", // name fo the root cli command
+	// about the commnad
+	Short: "txtBP is a automation tool to number the bullet points in a text file",
+	Long:  `txtBP is a automation tool to number the bullet points in a text file. Large`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			d = args[0]
+		}
+
+		internal.Start(d, e, s)
+	},
+}
+
+// Execute runs the main cli functions
+func Execute() {
+	// check for error
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err) // print the errror
+		os.Exit(1)       // exit the program
+	}
+}
+
+func init() {
+	// setting the flags
+	// getting the
+	rootCmd.PersistentFlags().StringVarP(&e, "extension", "e", ".txt", "File format of the files to read. Default=.txt")
+	rootCmd.PersistentFlags().StringVarP(&s, "sep", "s", ".txt", "Seperator of the line. Default='-'")
+}
